ac1d225b12de1c7a28c70b9eca721a4daa3aef7a
CarlosAl21 <calvarado2731@uta.edu.ec>
2024-04-09 10:53:22 -0500

    coso

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../Styles/EstilosNav.css'
+
+export default function Nav() {
+  return (
+    <div className='navegador'>
+      <div className='botones'>
+        <button>Home</button>
+        <button>Sitios turisticos</button>
+        <button>Contacto</button>
+      </div>
+      <div className='buscador'>
+        <input className='buscadorinput' type="text" />
+        <button className='buscadorbutton'>Buscar</button>
+        <button className='login'>Login</button>
+      </div>      
+    </div>
+  )
+}
