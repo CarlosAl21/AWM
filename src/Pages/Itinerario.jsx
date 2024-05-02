dfecdd13ede6ad130133adf6676b26de1da99dbd
CarlosAl21 <calvarado2731@uta.edu.ec>
2024-05-02 11:21:15 -0500

    Fin_?

--- src/Pages/Itinerario.jsx
+++ src/Pages/Itinerario.jsx
@@ -1,61 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../Components/Header";
 import "../Styles/EstilosItinerario.css";
+import Footer from "../Components/Footer";
 
 export default function Itinerario() {
-  const initialPlacesData = {
-    places: [
-      {
-        id: 1,
-        name: "Cascada de Agoyan",
-        description:
-          "Una impresionante cascada ubicada en las afueras de Baños, con una caída de agua de más de 60 metros.",
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/6/6a/Tarabita_-_Cascada_de_Agoy%C3%A1n.JPG",
-        schedule: "Todos los días: 9:00 am - 6:00 pm",
-      },
-      {
-        id: 2,
-        name: "Cascada Pailón del Diablo",
-        description:
-          "Una de las cascadas más famosas de Baños, conocida por su espectacular belleza natural y la oportunidad de caminar detrás de la cortina de agua.",
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/2/2c/Colage_pailon_del_diablo.jpg",
-        schedule: "Todos los días: 8:00 am - 5:00 pm",
-      },
-      {
-        id: 3,
-        name: "Mirador de la Virgen",
-        description:
-          "Un mirador ubicado en lo alto de una colina, desde donde se puede disfrutar de una vista panorámica de Baños y sus alrededores.",
-        image: "mirador-virgen.jpg",
-        schedule: "Todos los días: 10:00 am - 8:00 pm",
-      },
-      {
-        id: 4,
-        name: "Termas de la Virgen",
-        description:
-          "Un complejo de aguas termales con piscinas de diferentes temperaturas, ideal para relajarse y disfrutar de las propiedades curativas del agua.",
-        image: "termas-virgen.jpg",
-        schedule: "Todos los días: 7:00 am - 10:00 pm",
-      },
-      {
-        id: 5,
-        name: "Casa del Árbol",
-        description:
-          "Una casa construida al borde de un acantilado con una columpio conocido como 'el columpio del fin del mundo', que ofrece una emocionante experiencia y vistas impresionantes.",
-        image: "casa-arbol.jpg",
-        schedule: "Todos los días: 8:30 am - 7:00 pm",
-      },
-    ],
-  };
-
-  const [placesData, setPlacesData] = useState(initialPlacesData);
+  const [placesData, setPlacesData] = useState([]);
   const [itineraryData, setItineraryData] = useState({ places: [] });
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const fetchLugaresTuristicos = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/servicioLugar");
+        if (response.ok) {
+          const data = await response.json();
+          setPlacesData(data);
+        } else {
+          console.error("Error al cargar los datos de los lugares turísticos");
+        }
+      } catch (error) {
+        console.error("Error al conectar con la API:", error);
+      }
+    };
+
+    fetchLugaresTuristicos();
+  }, []);
 
   const addToItinerary = (place) => {
-    // Calculamos la hora de apertura como un número entero
-    const openingHour = parseInt(place.schedule.split(" ")[3]);
+    const openingHour = parseInt(place.Horarios.split(" ")[3]);
 
     const updatedItinerary = [
       ...itineraryData.places,
@@ -65,54 +37,121 @@ export default function Itinerario() {
     });
     setItineraryData({ places: updatedItinerary });
 
-    setPlacesData((prevData) => ({
-      places: prevData.places.filter((p) => p.id !== place.id),
-    }));
+    setPlacesData((prevData) => prevData.filter((p) => p.ID_Lugar !== place.ID_Lugar));
+    setShowButton(true);
   };
 
   const removeFromItinerary = (place) => {
-    setPlacesData((prevData) => ({
-      places: [...prevData.places, place],
-    }));
+    setPlacesData((prevData) => [...prevData, place]);
     setItineraryData((prevData) => ({
-      places: prevData.places.filter((p) => p.id !== place.id),
+      places: prevData.places.filter((p) => p.ID_Lugar !== place.ID_Lugar),
     }));
+    setShowButton(itineraryData.places.length > 1);
+  };
+
+  const generateItinerary = async () => {
+    const Correo_electronico = localStorage.getItem('Correo_electronico');
+    if (!Correo_electronico) {
+      alert('Inicia sesión para generar un itinerario');
+      return;
+    }
+  
+    try {
+      const responseUsuario = await fetch(`http://localhost:3000/servicioUsuario/email/${Correo_electronico}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:4000',
+        },
+      });
+  
+      if (responseUsuario.ok) {
+        const dataUsuario = await responseUsuario.json();
+        const ID_Usuario = dataUsuario.ID_Usuario;
+  
+        // Obtener solo los IDs de los lugares seleccionados
+        const lugaresSeleccionados = itineraryData.places.map(place => place.ID_Lugar);
+  
+        // Crear el objeto de itinerario con el ID del usuario
+        const itinerarioData = {
+          usuario: ID_Usuario,
+        };
+  
+        // Realizar la solicitud para crear el itinerario
+        const responseItinerario = await fetch(`http://localhost:3000/itinerarios/${ID_Usuario}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(itinerarioData),
+        });
+  
+        if (responseItinerario.ok) {
+          const dataItinerario = await responseItinerario.json();
+          const itinerarioId = dataItinerario.id;
+  
+          // Asociar los lugares turísticos al itinerario
+          for (const lugarId of lugaresSeleccionados) {
+            await fetch(`http://localhost:3000/itinerarios/${itinerarioId}/lugares/${lugarId}`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+          }
+  
+          console.log('Itinerario y lugares asociados con éxito');
+          window.location.href = '/itinerario';
+          // Realizar alguna acción adicional si es necesario
+        } else {
+          console.error('Error al generar el itinerario');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <>
+    <div className="itipage">
       <Header />
       <div className="itinerary">
         <h1>Itinerario</h1>
         <div className="itinerary-places">
-          {itineraryData.places.map((place) => (
-            <div key={place.id} className="place">
-              <img src={place.image} alt={place.name} />
-              <h3>{place.name}</h3>
-              <p>{place.description}</p>
-              <p>{place.schedule}</p>
+          {itineraryData.places && itineraryData.places.map((place) => (
+            <div key={place.ID_Lugar} className="place">
+              <img src={place.Imagen} alt={place.Nombre} />
+              <h3>{place.Nombre}</h3>
+              <p>{place.Descripcion}</p>
+              <span>{place.Horarios}</span>
               <button onClick={() => removeFromItinerary(place)}>
                 Eliminar del itinerario
               </button>
             </div>
           ))}
         </div>
+        {showButton && (
+          <div className="generate-itinerary">
+            <button onClick={generateItinerary}>Generar Itinerario</button>
+          </div>
+        )}
         <h2 style={{ textAlign: "center" }}>Lugares Disponibles</h2>
         <div className="places">
-          {placesData.places.map((place) => (
-            <div key={place.id} className="place">
-              <img src={place.image} alt={place.name} />
-              <h3>{place.name}</h3>
-              <p>{place.description}</p>
-              <p>{place.schedule}</p>
+          {placesData.map((place) => (
+            <div key={place.ID_Lugar} className="place">
+              <img src={place.Imagen} alt={place.Nombre} />
+              <h3>{place.Nombre}</h3>
+              <p>{place.Descripcion}</p>
+              <span>{place.Horarios}</span>
               <button onClick={() => addToItinerary(place)}>
                 Agregar al itinerario
               </button>
             </div>
           ))}
         </div>
-        <div className="details"></div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
+
